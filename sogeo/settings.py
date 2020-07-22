99240f7a7e26f8133d16c9ce735d03310584d10c
Moises Maia <dev.mmaia@gmail.com>
2020-07-22 18:45:19 -0300

    Changing db config for tests

--- sogeo/settings.py
+++ sogeo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import sys
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
 import django_heroku
@@ -87,9 +88,11 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-DATABASE_URL = os.environ.get('DATABASE_URL')
-db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
-DATABASES['default'].update(db_from_env)
+
+if not "test" in sys.argv:
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+    DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -138,4 +141,4 @@ REST_FRAMEWORK = {
 
 SESSION_COOKIE_DOMAIN = ""
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
